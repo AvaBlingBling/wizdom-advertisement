8ed968bf2349ad84fae7cf2865024fdb84e483c1
AvaBlingBling <251214798@qq.com>
2018-08-20 14:55:44 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Routers from './router';
 
 // import registerServiceWorker from './registerServiceWorker';
-import 'antd/dist/antd.less';
+// import 'antd/dist/antd.less';
 
 ReactDOM.render(<Routers />, document.getElementById('root'));
 // registerServiceWorker();
